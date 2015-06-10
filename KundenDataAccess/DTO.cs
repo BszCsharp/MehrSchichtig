e382b6de3c26018749901ed8c1e5dbd96afbb345
reinhold <reinhold@web.de>
2015-06-10 13:38:44 +0200

    DTO-Klasse Version1

--- KundenDataAccess/DTO.cs
+++ KundenDataAccess/DTO.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.OleDb;
 
 namespace KundenDataAccess
 {
     public class DTO
     {
-    }
+        OleDbConnection con = null;
+
+        private Boolean openConnection()
+        {
+            Boolean result = true;
+
+            return result;
+        }
+    }  
 }
