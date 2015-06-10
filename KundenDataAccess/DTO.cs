b8b262f16e5f7228d5dfd243d2cb343eb815c63b
Regu2 <reinguet@wb.de>
2015-06-10 13:57:26 +0200

    openConnection

--- KundenDataAccess/DTO.cs
+++ KundenDataAccess/DTO.cs
@@ -14,7 +14,16 @@ namespace KundenDataAccess
         private Boolean openConnection()
         {
             Boolean result = true;
-
+            con = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=D:\laptop_siemens\c#_public\KundenManage_ado\KundenDataAccess\kunden.mdb");
+            try 
+	        {	        
+		        con.Open();
+	        }
+	        catch (OleDbException )
+	        {
+		
+		       result = false;
+	        }
             return result;
         }
     }  
